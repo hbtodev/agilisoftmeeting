789ece83a7546f4afb64dc53f59b245c269ba714
hbtodev <humberto.s.santos@outlook.com>
2020-10-08 14:29:47 -0300

    ajuste na porta do server

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3000);
+app.set('port', (process.env.PORT || 3000));
+app.listen(app.get('port'), function() {
+    console.log('Server started on port '+ app.get('port'));
+});
 
-console.log("Aplicação rodando na porta 3000");
+// app.listen(3000);
+//console.log("Aplicação rodando na porta 3000");
